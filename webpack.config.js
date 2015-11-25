bdd7ca99240ecdcad6d2c884a36fd4f539fb0631
stephen grider <ste.grider@gmail.com>
2015-11-25 14:06:06 -0800

    squash all commits

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   entry: [
-    './src/main.js'
+    './src/index.js'
   ],
   output: {
     path: __dirname,
@@ -10,13 +10,17 @@ module.exports = {
   module: {
     loaders: [{
       exclude: /node_modules/,
-      loader: 'babel'
+      loader: 'babel',
+      query: {
+        presets: ['react', 'es2015', 'stage-1']
+      }
     }]
   },
   resolve: {
     extensions: ['', '.js', '.jsx']
   },
   devServer: {
+    historyApiFallback: true,
     contentBase: './'
   }
 };
